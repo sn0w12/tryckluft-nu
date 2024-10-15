a8176aa62001166b859394e7c8aa4ca993fe98c2
Lucas  Sköld <lucasskold49@gmail.com>
2024-10-15 17:20:10 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const email = "lucasskold49@gmail.com";
+
+function showSection() {
+    document.querySelectorAll("main section").forEach(section => {
+        section.style.display = "none";
+    });
+    const hash = window.location.hash;
+
+    if (hash) {
+        const section = document.querySelector(hash);
+        if (section) {
+            section.style.display = "block";
+        }
+    } else {
+        document.querySelector("#foretaget").style.display = "block";
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    showSection();
+    window.addEventListener("hashchange", showSection);
+});
